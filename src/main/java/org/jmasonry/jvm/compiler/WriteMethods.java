4c281abeafa563280a2e4717614e0d0bf1d813d3
Marek Żarnowski <marek1840@gmail.com>
2018-11-02 18:47:14 +0100

    #5 remove unnecessary classes

--- src/main/java/org/jmasonry/jvm/compiler/WriteMethods.java
+++
@@ -1,8 +0,0 @@
-package org.jmasonry.jvm.compiler;
-
-public class WriteMethods implements CompilationStep {
-    @Override
-    public void execute(CompilationUnitBuilder builder) {
-        builder.appendTwoBytes(0);      // currently methods are not supported
-    }
-}
