1275949120c350c072c84739982c0b02a0aa484c
Marek Żarnowski <marek1840@gmail.com>
2018-11-02 18:46:56 +0100

    #5 use class file factory during 'compilation'

--- src/main/java/org/jmasonry/jvm/compiler/DefaultCompiler.java
+++ src/main/java/org/jmasonry/jvm/compiler/DefaultCompiler.java
@@ -1,26 +1,22 @@
 package org.jmasonry.jvm.compiler;
 
+import org.jmasonry.jvm.classfile.ClassFile;
+import org.jmasonry.jvm.classfile.ClassFileFactory;
 import org.jmasonry.jvm.classfile.ClassFileVersion;
 import org.jmasonry.jvm.types.TypeDefinition;
 
-import java.util.List;
-
 public final class DefaultCompiler implements Compiler {
     private static final ClassFileVersion version = ClassFileVersion.JAVA_10;
+    private final ClassFileFactory classFileFactory = new ClassFileFactory(version);
 
     @Override
     public byte[] compile(TypeDefinition type) {
         CompilationUnitBuilder builder = CompilationUnitBuilder.createInMemory();
-        List<CompilationStep> compilationChain = createCompilationChain(type);
 
-        for (CompilationStep step : compilationChain) {
-            step.execute(builder);
-        }
+        ClassFile classFile = classFileFactory.create(type);
+        classFile.write(builder);
 
         return builder.build();
     }
 
-    private List<CompilationStep> createCompilationChain(TypeDefinition type) {
-        return new CompilationChain(version, type).getSteps();
-    }
 }
