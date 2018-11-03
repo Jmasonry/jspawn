564f8291710f5dc0097749f86adf97ceb50660fe
Marek Żarnowski <marek1840@gmail.com>
2018-11-03 19:37:27 +0100

    #4 simplify tests

--- src/test/java/org/jmasonry/jvm/jspawn/SimpleClassSpawnTest.java
+++ src/test/java/org/jmasonry/jvm/jspawn/SimpleClassSpawnTest.java
@@ -1,29 +1,51 @@
 package org.jmasonry.jvm.jspawn;
 
 import org.jmasonry.jvm.types.FieldDeclaration;
+import org.jmasonry.jvm.types.MethodDefinition;
 import org.jmasonry.jvm.types.Type;
 import org.jmasonry.jvm.types.TypeDeclaration;
 import org.jmasonry.jvm.types.TypeDefinition;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
-import java.util.Collections;
 
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.jmasonry.jvm.types.TypeDeclaration.create;
 
 class SimpleClassSpawnTest extends SpawnAbstractTest {
     private static final TypeDeclaration TYPE_DECLARATION = TypeDeclaration.create(SELF_TYPE, Type.of(Object.class));
 
     @Test
-    void spawns_simple_class() {
+    void spawns_a_class() {
         // given
-        TypeDefinition definition = TypeDefinition.of(TYPE_DECLARATION);
+        Type superClass = Type.of(Foo.class);
+        TypeDeclaration declaration = TypeDeclaration.create(SELF_TYPE, superClass);
+        MethodDefinition defaultConstructor = defaultConstructor(superClass);
+
+        TypeDefinition definition = TypeDefinition.of(declaration, emptyList(), singletonList(defaultConstructor));
 
         // when
-        Class<?> spawnedClass = nest.spawn(definition);
+        Class<?> spawned = nest.spawn(definition);
 
         // then
-        assertThat(spawnedClass.getName()).isEqualTo(SELF_TYPE.getName());
+        assertThat(Foo.class).isAssignableFrom(spawned);
+    }
+
+    @Test
+    void spawns_implementation() {
+        // given
+        Type superClass = Type.of(Object.class);
+        TypeDeclaration declaration = create(SELF_TYPE, superClass, Type.of(IFace.class), Type.of(OtherIFace.class));
+        TypeDefinition definition = TypeDefinition.of(declaration);
+
+        // when
+        Class<?> spawned = nest.spawn(definition);
+
+        // then
+        assertThat(IFace.class).isAssignableFrom(spawned);
+        assertThat(OtherIFace.class).isAssignableFrom(spawned);
     }
 
     @Test
@@ -32,13 +54,24 @@ class SimpleClassSpawnTest extends SpawnAbstractTest {
         String fieldName = "foo";
         Type fieldType = Type.of(Integer.class);
         FieldDeclaration field = new FieldDeclaration(fieldName, fieldType);
-        TypeDefinition definition = TypeDefinition.of(TYPE_DECLARATION, Collections.singletonList(field));
+        TypeDefinition definition = TypeDefinition.of(TYPE_DECLARATION, singletonList(field), emptyList());
 
         // when
-        Class<?> spawnedClass = nest.spawn(definition);
+        Class<?> spawned = nest.spawn(definition);
 
         // then
-        Field declaredField = spawnedClass.getDeclaredField(fieldName);
-        assertThat(declaredField.getType().getTypeName()).isEqualTo(fieldType.getName());
+        Field declaredField = spawned.getDeclaredField(fieldName);
+        assertThat(declaredField.getType()).isEqualTo(Integer.class);
+    }
+
+    // must be public
+    public interface IFace {}
+
+    public interface OtherIFace {}
+
+    // all must be public
+    public static class Foo {
+        public Foo() {
+        }
     }
 }
