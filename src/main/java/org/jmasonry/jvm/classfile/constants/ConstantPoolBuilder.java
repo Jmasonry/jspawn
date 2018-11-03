ee38a65aadad062536c5cf0d2ae4f54aadc25c2e
Marek Żarnowski <marek1840@gmail.com>
2018-11-03 19:39:46 +0100

    #4 introduce method-related constants

--- src/main/java/org/jmasonry/jvm/classfile/constants/ConstantPoolBuilder.java
+++ src/main/java/org/jmasonry/jvm/classfile/constants/ConstantPoolBuilder.java
@@ -1,13 +1,13 @@
 package org.jmasonry.jvm.classfile.constants;
 
 
+import org.jmasonry.jvm.types.MethodDeclaration;
 import org.jmasonry.jvm.types.Type;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import static org.jmasonry.jvm.classfile.constants.ConstantFactory.classConst;
-import static org.jmasonry.jvm.classfile.constants.ConstantFactory.utf8Const;
+import static org.jmasonry.jvm.classfile.constants.ConstantFactory.*;
 
 public final class ConstantPoolBuilder {
     private final Map<Constant, Integer> cache = new LinkedHashMap<>();
@@ -17,16 +17,27 @@ public final class ConstantPoolBuilder {
         return getOrCreate(constant);
     }
 
-    public int appendDescriptor(Type type) {
-        String descriptor = type.getDescriptor();
-        return appendUTF8(descriptor);
-    }
-
     public int appendClass(Type type) {
-        String typeName = type.getInternalName();
+        var typeName = type.getInternalName();
         var nameIndex = appendUTF8(typeName);
 
         var constant = classConst(nameIndex);
+        return getOrCreate(constant);
+    }
+
+    private int appendNameAndType(String name, String descriptor) {
+        var nameIndex = appendUTF8(name);
+        var descriptorIndex = appendUTF8(descriptor);
+
+        var constant = nameAndType(nameIndex, descriptorIndex);
+        return getOrCreate(constant);
+    }
+
+    public int appendMethod(Type classType, MethodDeclaration methodDeclaration) {
+        var classIndex = appendClass(classType);
+        var nameAndTypeIndex = appendNameAndType(methodDeclaration.getName(), methodDeclaration.getDescriptor());
+
+        var constant = methodRef(classIndex, nameAndTypeIndex);
         return getOrCreate(constant);
     }
 
